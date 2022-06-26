fc859437173ac7e33ade3098aa86ad80975bcd15
Aiden Lee <team.k0konutz@gmail.com>
2022-06-26 22:18:34 +0900

    TEXT ELEPHANT - Web Scraper 0.1v
    Settings Function Completed

--- command/banner.py
+++ command/banner.py
@@ -1,9 +1,4 @@
-import argparse
-import os
-
 from colorama import Fore
-
-from database.crud import do_sql
 
 
 def banner():
@@ -15,57 +10,3 @@ def banner():
    ██║   ███████╗██╔╝ ██╗   ██║       ███████╗███████╗███████╗██║     ██║  ██║██║  ██║██║ ╚████║   ██║   
    ╚═╝   ╚══════╝╚═╝  ╚═╝   ╚═╝       ╚══════╝╚══════╝╚══════╝╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝   ╚═╝   
 Made {Fore.RED}♥{Fore.GREEN} by Aiden Lee(@Twoicefish) {Fore.CYAN}''')
-
-
-def cls():
-    __command = 'clear'
-
-    if os.name in ('nt', 'dos'):
-        __command = 'cls'
-
-    os.system(__command)
-    banner()
-
-
-def __templates():
-    # show templates list
-    selected_template = ''
-    template_list = do_sql('select * from template_list')
-    for i in template_list:
-        print(i)
-        selected_template = i
-
-    # show template info
-    template = input('Choose template number > ')
-    template_info = do_sql(f'select * from "{selected_template[1]}" where no = {template}')
-    if len(template_info) > 0:
-        cls()
-        print(f'{Fore.RED}{i[1]}{Fore.CYAN}')
-        print(f'{"No":-<4}{"Column":-<10}{"Selector":-<60}{"x_len":-<10}{"x":-<4}{"Test":-<10}')
-        for ii in template_info:
-            print(f'{ii[0]: <4}{ii[1]: <10}{ii[2]: <60}{ii[3]: <10}{ii[4]: <4}{ii[5]: <10}')
-
-    else:
-        cls()
-        print(f'{Fore.RED}{i[1]}{Fore.CYAN}')
-        print('No data')
-
-
-def __options():
-    option_list = do_sql('select * from settings')
-    print(f'{Fore.RED}Show Run options{Fore.CYAN}')
-    print(f'{"No":-<4}{"Key":-<20}{"Value":-<20}')
-    for i in option_list:
-        print(f'{i[0]: <4}{i[1]: <20}{i[2]: <20}')
-
-
-parser = argparse.ArgumentParser(prog='show', usage='%(prog)s [options]')
-parser.add_argument('options', choices=['templates', 'options'])
-
-args = parser.parse_args()
-
-if args.options == 'templates':
-    __templates()
-
-if args.options == 'options':
-    __options()
--- command/set.py
+++ command/set.py
@@ -4,22 +4,12 @@ import re
 
 from colorama import Fore
 
+from banner import banner
 from database.crud import do_sql
 
 SELECT_ALL_FROM_TEMPLATE_LIST = f'select * from template_list'
 SELECT_ALL_FROM_SETTINGS = 'select * from settings'
 SETTINGS_COLUMNS = f'{"No":-<4}{"Key":-<20}{"Value":-<30}{"Comments":-<20}'
-
-
-def banner():
-    print(Fore.GREEN + f'''
-████████╗███████╗██╗  ██╗████████╗    ███████╗██╗     ███████╗██████╗ ██╗  ██╗ █████╗ ███╗   ██╗████████╗
-╚══██╔══╝██╔════╝╚██╗██╔╝╚══██╔══╝    ██╔════╝██║     ██╔════╝██╔══██╗██║  ██║██╔══██╗████╗  ██║╚══██╔══╝
-   ██║   █████╗   ╚███╔╝    ██║       █████╗  ██║     █████╗  ██████╔╝███████║███████║██╔██╗ ██║   ██║   
-   ██║   ██╔══╝   ██╔██╗    ██║       ██╔══╝  ██║     ██╔══╝  ██╔═══╝ ██╔══██║██╔══██║██║╚██╗██║   ██║   
-   ██║   ███████╗██╔╝ ██╗   ██║       ███████╗███████╗███████╗██║     ██║  ██║██║  ██║██║ ╚████║   ██║   
-   ╚═╝   ╚══════╝╚═╝  ╚═╝   ╚═╝       ╚══════╝╚══════╝╚══════╝╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝   ╚═╝   
-Made {Fore.RED}♥{Fore.GREEN} by Aiden Lee(@Twoicefish) {Fore.CYAN}''')
 
 
 def cls():
@@ -29,6 +19,7 @@ def cls():
         __command = 'cls'
 
     os.system(__command)
+
     banner()
 
 
@@ -45,73 +36,80 @@ def clear_show():
     show_options()
 
 
+def __options():
+    selected_option = input(f'{Fore.RED}Change No [q] > {Fore.GREEN}')
+
+    if selected_option == 'q':
+        quit()
+    try:
+        cls()
+        # show before data
+        before_data = do_sql(f'select * from settings where no = {int(selected_option)}')[0]
+        print(SETTINGS_COLUMNS)
+        print(
+            f'{before_data[0]: <4}{before_data[1]: <20}{Fore.RED}{before_data[2]: <30}{Fore.GREEN}{before_data[3]: <20}')
+
+        if selected_option == '1':
+            # show templates
+            template_list = do_sql(SELECT_ALL_FROM_TEMPLATE_LIST)
+            print('Templates : ' + str(template_list))
+
+            try:
+                new_value = input(f'{Fore.RED}Choose Template No > {Fore.GREEN}')
+                template_name = do_sql(f'select name from template_list where no ={str(new_value)}')[0][0]
+                do_sql(f'update settings set value="{template_name}" where no = {int(selected_option)}')
+                clear_show()
+                return
+            except IndexError:
+                return
+
+        if selected_option == '2' or selected_option == '3' or selected_option == '4':
+            new_value = input(f'{Fore.RED}New Value > {Fore.GREEN}')
+
+            if selected_option == '2':
+                if len(new_value) == 0:
+                    return
+                new_value = new_value.strip()
+
+                new_value = re.sub(f'[^a-zA-z0-9.]+', '', new_value)
+                new_value = new_value.replace('.xlsx', '')
+
+            if selected_option == '3':
+                try:
+                    if int(new_value) > 60:
+                        new_value = "60"
+                except ValueError:
+                    return
+
+            if selected_option == '4':
+
+                if '@' not in new_value:
+                    return
+
+            do_sql(f'update settings set value="{new_value}" where no = {int(selected_option)}')
+            clear_show()
+
+    except ValueError:
+        pass
+
+
+def __templates():
+    cls()
+    selected_option = input(f'{Fore.RED}To No [q] > {Fore.GREEN}')
+    if selected_option == 'q':
+        exit()
+
+
 parser = argparse.ArgumentParser(prog='set', usage='%(prog)s [options]')
 parser.add_argument('options', choices=['templates', 'options'])
 
 args = parser.parse_args()
 
-
-def start():
-    clear_show()
-    if args.options == 'templates':
-        selected_option = input(f'{Fore.RED}Change No [q] > {Fore.GREEN}')
-    if args.options == 'options':
-
-        selected_option = input(f'{Fore.RED}Change No [q] > {Fore.GREEN}')
-
-        if selected_option == 'q':
-            quit()
-        try:
-            cls()
-            # show before data
-            before_data = do_sql(f'select * from settings where no = {int(selected_option)}')[0]
-            print(SETTINGS_COLUMNS)
-            print(
-                f'{before_data[0]: <4}{before_data[1]: <20}{Fore.RED}{before_data[2]: <30}{Fore.GREEN}{before_data[3]: <20}')
-
-            if selected_option == '1':
-                # show templates
-                template_list = do_sql(SELECT_ALL_FROM_TEMPLATE_LIST)
-                print('Templates : ' + str(template_list))
-
-                try:
-                    new_value = input(f'{Fore.RED}Choose Template No > {Fore.GREEN}')
-                    template_name = do_sql(f'select name from template_list where no ={str(new_value)}')[0][0]
-                    do_sql(f'update settings set value="{template_name}" where no = {int(selected_option)}')
-                    clear_show()
-                    return
-                except IndexError:
-                    return
-
-            if selected_option == '2' or selected_option == '3' or selected_option == '4':
-                new_value = input(f'{Fore.RED}New Value > {Fore.GREEN}')
-
-                if selected_option == '2':
-                    if len(new_value) == 0:
-                        return
-                    new_value = new_value.strip()
-
-                    new_value = re.sub(f'[^a-zA-z0-9.]+', '', new_value)
-                    new_value = new_value.replace('.xlsx', '')
-
-                if selected_option == '3':
-                    try:
-                        if int(new_value) > 60:
-                            new_value = "60"
-                    except ValueError:
-                        return
-
-                if selected_option == '4':
-
-                    if '@' not in new_value:
-                        return
-
-                do_sql(f'update settings set value="{new_value}" where no = {int(selected_option)}')
-                clear_show()
-
-        except ValueError:
-            pass
-
-
 while True:
-    start()
+
+    if args.options == 'templates':
+        cls()
+        __templates()
+    if args.options == 'options':
+        clear_show()
+        __options()
